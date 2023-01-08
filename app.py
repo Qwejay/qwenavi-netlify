5c2e65295ca9bd8d60db03f940f24e22676be15e
QwejayHuang <dkm382638608@163.com>
2023-01-08 09:19:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,7 @@
 
 from flask import Flask
 app = Flask(__name__)
- 
-@app.route("/")
-def hello():
-    return "hi"
 
-@app.route('/hi')
+@app.route('/')
 def index():
     return render_template('hello.html')
