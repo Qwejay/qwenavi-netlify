1ffc922a62c5dd14d64873e2923e7c1c7d2ad674
QwejayHuang <dkm382638608@163.com>
2023-01-08 18:35:40 +0800

    master

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from flask import Flask, render_template
+from flask_frozen import Freezer
 
-from flask import Flask
+
 app = Flask(__name__)
+freezer = Freezer(app)
 
-@app.route('/')
-def index():
-    return 'Index Page'
 
-@app.route('/hello')
-def hello():
-    return 'Hello World'
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        print("Building website...")
+        freezer.freeze()
+    else:
+        app.run(debug=True)
